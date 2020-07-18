f6b25fbc2f5bd1d7d9eaa22114c7cfb310f2fae6
howard <howard104023060@icloud.com>
2020-07-18 22:15:44 +0800

    calc

--- review.py
+++ review.py
@@ -6,5 +6,13 @@ with open('reviews.txt', 'r')as f:
 		count += 1
 		if count % 1000 == 0:
 		    print(len(data))
-print(len(line))
-print(data[0])
+print('檔案讀完了，共有', len(line), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len / len(data))
+
+
+
